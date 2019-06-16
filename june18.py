0a2404220b743f9429a505cdcae2fd2737369eee
shajia1234 <32437442+shajia1234@users.noreply.github.com>
2019-06-16 18:05:23 +0500

    Update june18.py

--- june18.py
+++ june18.py
@@ -1,24 +1,24 @@
-# a=int(input("enter 1st no."))
-# b=int(input("enter 2nd no."))
-# def add(num1 , num2):
-#     sum=num1+num2
-#     print(sum)
-# add(a,b)
-# def sub(num1 , num2):
-#     minus=num1-num2
-#     print(minus)
-# def mult(num1 , num2):
-#     prdct=num1*num2
-#     print(prdct)
-# def div(num1 , num2):
-#     divide=num1/num2
-#     print(divide)
-# sub(a,b)
-# mult(a,b)
-# div(a,b)
-# def my_pet(owner , pet):
-#     print(owner,"is an owner of", pet)
-# my_pet(owner="john",pet="cat")
+ a=int(input("enter 1st no."))
+ b=int(input("enter 2nd no."))
+ def add(num1 , num2):
+     sum=num1+num2
+     print(sum)
+ add(a,b)
+ def sub(num1 , num2):
+     minus=num1-num2
+     print(minus)
+ def mult(num1 , num2):
+     prdct=num1*num2
+     print(prdct)
+ def div(num1 , num2):
+     divide=num1/num2
+     print(divide)
+ sub(a,b)
+ mult(a,b)
+ div(a,b)
+ def my_pet(owner , pet):
+     print(owner,"is an owner of", pet)
+ my_pet(owner="john",pet="cat")
 def chk_num(number):
     if number % 2 == 0:
         print("number is even")
